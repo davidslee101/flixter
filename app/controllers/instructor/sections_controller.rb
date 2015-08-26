d87f88fe758da5ead817df41476e2708d219514f
david <davidslee101@gmail.com>
2015-08-26 02:53:42 +0000

    fixed lesson ordering_I_AM_DONE

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -35,8 +35,8 @@ class Instructor::SectionsController < ApplicationController
 
   helper_method :current_course
   def current_course
-    # @current_course ||= Course.find(params[:course_id])
-    current_section.course
+    @current_course ||= Course.find(params[:course_id])
+    # current_section.course
   end
 
   def section_params
